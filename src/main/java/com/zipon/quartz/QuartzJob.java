01b901884e8adff533fb09019a5b3022a24a5200
zipon <1418641603@qq.com>
2016-12-17 21:02:08 +0800

    加入定时任务。

--- src/main/java/com/zipon/quartz/QuartzJob.java
+++ src/main/java/com/zipon/quartz/QuartzJob.java
@@ -10,6 +10,7 @@ public class QuartzJob {
 
         public void work()
         {
+
             System.out.println("Quartz的任务调度！！！");
         }
     }
---
+++ src/main/java/com/zipon/test/Log.java
@@ -0,0 +1,38 @@
+package com.zipon.test;
+
+/**
+ * Created by zipon on 2016/11/16.
+ */
+
+import org.apache.log4j.*;
+
+import java.io.File;
+
+public class Log {
+    public  Logger logger;
+    public static String log4jdir;
+    private static String filePath = "src/log4j.properties";
+    public static String loggerName = "test_log";
+
+    public Log(String name){
+        loggerName = name;
+        logger = Logger.getLogger(loggerName);
+        log4jdir = System.getenv( "log4jdir" );
+        if(log4jdir==null)
+            log4jdir = "testlog";
+        System.setProperty("log4jdir", log4jdir);
+        PropertyConfigurator.configure(new File(filePath).getAbsolutePath());
+    }
+    public Log(){
+
+        logger = Logger.getLogger(loggerName);
+        log4jdir = System.getenv( "log4jdir" );
+        if(log4jdir==null)
+            log4jdir = "testlog";
+        System.setProperty("log4jdir", log4jdir);
+        PropertyConfigurator.configure(new File(filePath).getAbsolutePath());
+    }
+
+
+
+}
